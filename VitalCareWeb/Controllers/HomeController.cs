3b1a27a4a1979a392f08b14ed8e4ed8eb432f970
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-10-13 23:29:12 +0500

    Add project files.

---
+++ VitalCareWeb/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VitalCareWeb.Models;
+
+namespace VitalCareWeb.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult AboutUs()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Services()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Doctors()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Articles()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
